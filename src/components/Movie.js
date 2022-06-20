c9e66574bd3421a41c319e53fa33a782ee464368
wenhuang1 <huangwen@login.khoury.neu.edu>
2022-06-20 19:05:20 -0400

    fail to compile

---
+++ src/components/Movie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Movie = props => {
+    return (
+        <div className = "App" >
+        Placeholder text for Movie 
+        </div>
+    )
+}
+
+export default Movie;
