9355985c7a11e64375de384d60efa507a3d681aa
wenhuang1 <huangwen@login.khoury.neu.edu>
2022-07-24 21:33:24 -0400

    test-for-heroku

--- src/components/MyFavorites.js
+++ src/components/MyFavorites.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { render } from 'react-dom';
-// import Example from './example';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Container from './Container.js'
